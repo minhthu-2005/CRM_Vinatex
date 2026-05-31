cd0f6004e85ae9809377bb91e3a00b7ff650cab2
thxuanng <pinkpig01012005@gmail.com>
2026-05-31 17:37:31 +0700

    Add files via upload

---
+++ campaign_personalization/models/__init__.py
@@ -0,0 +1,4 @@
+from . import res_partner_ext
+from . import mail_template_ext
+from . import mail_campaign_ext
+from . import loyalty_rule_wizard
---
+++ campaign_personalization/models/mail_campaign_ext.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+
+class MailCampaign(models.Model):
+    _inherit = "custom.mail.campaign"
